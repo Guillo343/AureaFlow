cdd0f97a282bfc8108539f03b159050ef96c94c2
Guillermo Muñoz <guillemunozi2003@gmail.com>
2025-12-18 16:43:32 -0500

    Update SplashScreen.tsx

--- aureaflow/src/components/loading/SplashScreen.tsx
+++ aureaflow/src/components/loading/SplashScreen.tsx
@@ -6,7 +6,7 @@ export default function SplashScreen() {
       initial={{ opacity: 1 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 0.6, ease: "easeOut" }}
+      transition={{ duration: 0.4, ease: "easeOut" }}
       className="w-full h-screen flex flex-col items-center justify-center bg-white"
     >
       <motion.h1
